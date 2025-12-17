06e25eea9ce98f4a8df6291e401f342bdf57d3c6
Owoyele Adedayo <67150149+Owoyeleadedayo@users.noreply.github.com>
2025-12-17 11:01:47 +0100

    fixed tailwind error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,13 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        oleo: ["var(--font-oleo)", "cursive"], 
-        montserrat: ["var(--font-montserrat)", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;

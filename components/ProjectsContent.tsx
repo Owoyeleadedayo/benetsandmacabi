c5b9d589d4bfe5d8f3e3a592828b7c428e96deff
Owoyele Adedayo <67150149+Owoyeleadedayo@users.noreply.github.com>
2025-12-31 23:46:22 +0100

    worked project page

--- components/ProjectsContent.tsx
+++ components/ProjectsContent.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const ProjectsContent = () => {
   return (
-    <div className="flex flex-col items-center bg-[#EBEBEB]">
-      <div className="flex flex-col pt-5">
+    <div className="flex flex-col bg-[#EBEBEB]">
+      {/* <div className="flex flex-col pt-5">
         <div className="relative flex flex-col lg:flex-row gap-6">
           <div className="relative w-100 md:w-150 h-70 md:h-117">
             <Image
@@ -36,35 +36,34 @@ const ProjectsContent = () => {
       </div>
 
       <div className="flex flex-col px-5 md:px-0 pt-5 md:pt-0">
-      <div className="relative flex flex-col lg:flex-row gap-6">
-        <div className="relative w-46 h-117 hidden md:hidden lg:block overflow-hidden">
-          <Image
-            src="/img/f9.jpg"
-            alt="project"
-            fill
-            className="object-cover"
-          />
-        </div>
+        <div className="relative flex flex-col lg:flex-row gap-6">
+          <div className="relative w-46 h-117 hidden md:hidden lg:block overflow-hidden">
+            <Image
+              src="/img/f9.jpg"
+              alt="project"
+              fill
+              className="object-cover"
+            />
+          </div>
 
-        <div className="relative w-100 md:w-150 h-70 md:h-117 px-8">
-          <Image
-            src="/img/f6.jpg"
-            alt="project"
-            fill
-            className="object-cover"
-          />
-        </div>
+          <div className="relative w-100 md:w-150 h-70 md:h-117 px-8">
+            <Image
+              src="/img/f6.jpg"
+              alt="project"
+              fill
+              className="object-cover"
+            />
+          </div>
 
-        <div className="relative w-100 md:w-150 h-70 md:h-117 overflow-hidden">
-          <Image
-            src="/img/f7.jpg"
-            alt="project"
-            fill
-            className="object-cover"
-          />
+          <div className="relative w-100 md:w-150 h-70 md:h-117 overflow-hidden">
+            <Image
+              src="/img/f7.jpg"
+              alt="project"
+              fill
+              className="object-cover"
+            />
+          </div>
         </div>
-      </div>
-
       </div>
 
       <div className="flex flex-col pt-5">
@@ -98,39 +97,259 @@ const ProjectsContent = () => {
         </div>
       </div>
 
+
+
+     
+
       <div className="flex flex-col px-5 md:px-0 pt-5 md:pt-0">
-      <div className="relative flex flex-col lg:flex-row gap-6">
-        <div className="relative w-46 h-117 hidden md:hidden lg:block overflow-hidden">
-          <Image
-            src="/img/f3.jpg"
-            alt="project"
-            fill
-            className="object-cover"
-          />
+        <div className="relative flex flex-col lg:flex-row gap-6">
+          <div className="relative w-46 h-117 hidden md:hidden lg:block overflow-hidden">
+            <Image
+              src="/img/f3.jpg"
+              alt="project"
+              fill
+              className="object-cover"
+            />
+          </div>
+
+          <div className="relative w-100 md:w-150 h-70 md:h-117 px-8">
+            <Image
+              src="/img/f8.jpg"
+              alt="project"
+              fill
+              className="object-cover"
+            />
+          </div>
+
+          <div className="relative w-100 md:w-150 h-70 md:h-117 overflow-hidden">
+            <Image
+              src="/img/f12.jpg"
+              alt="project"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+      </div> */}
+
+      <div className="flex flex-col pb-20 px-20 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-10 ">
+          <div className="relative w-150 h-162.5">
+            <Image
+              src="/img/pro1.jpg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="relative w-full h-75">
+              <Image
+                src="/img/pro2.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <div className="relative w-full h-75 ">
+              <Image
+                src="/img/pro3.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <div className="relative w-full h-81.25 ">
+              <Image
+                src="/img/pro5.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <div className="relative w-full h-81.25 ">
+              <Image
+                src="/img/pro6.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
         </div>
 
-        <div className="relative w-100 md:w-150 h-70 md:h-117 px-8">
-          <Image
-            src="/img/f8.jpg"
-            alt="project"
-            fill
-            className="object-cover"
-          />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="relative w-150 h-85">
+            <Image
+              src="/img/pro11.jpg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-150 h-85">
+            <Image
+              src="/img/pro12.jpg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-150 h-85">
+            <Image
+              src="/img/pro13.jpg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-150 h-85">
+            <Image
+              src="/img/pro14.jpg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
         </div>
 
-        <div className="relative w-100 md:w-150 h-70 md:h-117 overflow-hidden">
-          <Image
-            src="/img/f12.jpg"
-            alt="project"
-            fill
-            className="object-cover"
-          />
-        </div>
-      </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-10 ">
+          <div className="relative w-150 h-162.5">
+            <Image
+              src="/img/pro24.jpg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-6">
+            <div className="relative w-full h-75">
+              <Image
+                src="/img/pro25.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
 
+            <div className="relative w-full h-75 ">
+              <Image
+                src="/img/pro26.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <div className="relative w-full h-81.25 ">
+              <Image
+                src="/img/pro27.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <div className="relative w-full h-81.25 ">
+              <Image
+                src="/img/pro28.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
+        </div>
+
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="relative w-150 h-85">
+            <Image
+              src="/img/banner1.jpg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-150 h-85">
+            <Image
+              src="/img/serr.jpg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-150 h-85">
+            <Image
+              src="/img/abb.jpeg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative w-150 h-85">
+            <Image
+              src="/img/abbs.jpg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-10 ">
+          <div className="relative w-150 h-162.5">
+            <Image
+              src="/img/pro35.jpeg"
+              alt="display"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-6">
+            <div className="relative w-full h-75">
+              <Image
+                src="/img/pro32.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <div className="relative w-full h-75 ">
+              <Image
+                src="/img/pro34.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <div className="relative w-full h-81.25 ">
+              <Image
+                src="/img/pro33.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <div className="relative w-full h-81.25 ">
+              <Image
+                src="/img/pro36.jpg"
+                alt="display"
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectsContent
+export default ProjectsContent;

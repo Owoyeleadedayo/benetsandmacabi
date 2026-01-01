49a86917628ac3b45a42cbbec18ea84909c4ab70
Owoyele Adedayo <67150149+Owoyeleadedayo@users.noreply.github.com>
2026-01-01 11:46:23 +0100

    fixed newsletter

--- components/Celebrate.tsx
+++ components/Celebrate.tsx
@@ -7,7 +7,7 @@ const Celebrate = () => {
       <div className="flex relative h-50 md:h-112.5 bg-[url('/img/banner.jpg')] bg-no-repeat bg-cover bg-center">
         <div className="absolute inset-0 bg-black/70"></div>
         <div className="absolute flex flex-col w-full h-full justify-center items-center px-10 md:px-20 pt-10 gap-2 md:gap-4">
-          <p className="text-2xl max-w-70 md:max-w-xl md:text-5xl text-[#FFF] font-semibold font-(family-name:--font-quattrocento) text-center capitalize">
+          <p className="text-2xl w-full md:max-w-xl md:text-5xl text-[#FFF] font-semibold font-(family-name:--font-quattrocento) text-center capitalize">
             Let’s Celebrate Your Event
           </p>
           <div>

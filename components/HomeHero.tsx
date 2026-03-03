7892e341fe45a3a48bcedc91de9adbaa56afa9ea
Owoyele Adedayo <67150149+Owoyeleadedayo@users.noreply.github.com>
2026-03-04 00:01:37 +0100

    feat:added video to the homepage

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -16,49 +16,57 @@ const HomeHero = () => {
   return (
     <>
       <div>
-        <Carousel
-          autoplay={{ dotDuration: true }}
-          autoplaySpeed={5000}
-          effect="fade"
-          dots
-        >
-          {banners.map((img, index) => (
-            <div key={index}>
-              <div
-                className="relative min-h-[70vh] md:min-h-[85vh] bg-no-repeat bg-cover bg-fixed bg-center"
-                style={{ backgroundImage: `url(${img})` }}
-              >
-                <div className="absolute inset-0 bg-black/60"></div>
-                <div className="absolute flex flex-col w-full h-full justify-center items-center px-10 md:px-20 gap-3">
-                  <motion.p
-                    initial={{ opacity: 0, y: 30 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8, ease: "easeOut" }}
-                    className="text-2xl max-w-75 md:max-w-3xl md:text-6xl text-white text-center capitalize leading-6 md:leading-12 font-medium font-(family-name:--font-quattrocento)"
-                  >
-                    Moments into Memories Since 2009
-                  </motion.p>
+      <Carousel
+  autoplay={{ dotDuration: true }}
+  autoplaySpeed={5000}
+  effect="fade"
+  dots
+>
+  <div>
+    <div className="relative min-h-[70vh] md:min-h-[85vh] overflow-hidden">
 
-                  <motion.div
-                    initial={{ opacity: 0, scale: 0.9 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ delay: 0.4, duration: 0.5 }}
-                  >
-                    <Link href={"/contact"}>
-                      <Button
-                        className="bg-[#0071B3] text-white text-base md:text-xl cursor-pointer transition-all duration-300 ease-out
-    hover:scale-105 hover:bg-[#005f99]
-    active:scale-95"
-                      >
-                        Plan Your Events
-                      </Button>
-                    </Link>
-                  </motion.div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </Carousel>
+      {/* Background Video */}
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/video/mandb.mp4" type="video/mp4" />
+      </video>
+
+      <div className="absolute inset-0 bg-black/70 z-10"></div>
+
+      <div className="absolute flex flex-col w-full h-full justify-center items-center px-10 md:px-20 gap-3 z-20">
+        <motion.p
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="text-2xl max-w-75 md:max-w-3xl md:text-6xl text-white text-center capitalize leading-6 md:leading-12 font-medium font-(family-name:--font-quattrocento)"
+        >
+          Moments into Memories Since 2009
+        </motion.p>
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+        >
+          <Link href={"/contact"}>
+            <Button
+              className="bg-[#0071B3] text-white text-base md:text-xl cursor-pointer transition-all duration-300 ease-out
+                hover:scale-105 hover:bg-[#005f99]
+                active:scale-95"
+            >
+              Plan Your Events
+            </Button>
+          </Link>
+        </motion.div>
+      </div>
+    </div>
+  </div>
+</Carousel>
       </div>
       <AboutUs />
       <OurServices />

04e8990cc1e7e235457c0c29530ca4ff285def9a
Owoyele Adedayo <67150149+Owoyeleadedayo@users.noreply.github.com>
2026-08-18 09:40:09 +0100

    feat: replace static YouTube thumbnails with dynamic links and enable remote image optimization

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -120,54 +120,45 @@ const Footer = () => {
         <div className="col-span-2 flex flex-col gap-4 ml-0 xl:ml-20">
           <p className="text-white text-lg font-medium">Youtube</p>
           <div className="grid grid-cols-3 gap-2">
-            <div className="relative w-full h-21.25">
-              <Image
-                src="/img/f2.jpg"
-                alt="footer"
-                fill
-                className="object-cover"
-              />
-            </div>
-            <div className="relative w-full h-21.25">
-              <Image
-                src="/img/f4.jpg"
-                alt="footer"
-                fill
-                className="object-cover"
-              />
-            </div>
-            <div className="relative w-full h-21.25">
-              <Image
-                src="/img/f1.jpg"
-                alt="footer"
-                fill
-                className="object-cover"
-              />
-            </div>
-            <div className="relative w-full h-21.25">
-              <Image
-                src="/img/f5.jpg"
-                alt="footer"
-                fill
-                className="object-cover d"
-              />
-            </div>
-            <div className="relative w-full h-21.25">
-              <Image
-                src="/img/team.jpg"
-                alt="footer"
-                fill
-                className="object-cover"
-              />
-            </div>
-            <div className="relative w-full h-21.25">
-              <Image
-                src="/img/f3.jpg"
-                alt="footer"
-                fill
-                className="object-cover"
-              />
-            </div>
+            {[
+              "qB6xWdaVuTs",
+              "2FwNu5rh6-U",
+              "4j3e1HJc05E",
+              "97h3iEg705M",
+              "tmHL-ht8NcI",
+              "BoM5cPLYQoY",
+            ].map((id) => (
+              <a
+                key={id}
+                href={`https://www.youtube.com/watch?v=${id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative w-full h-21.25 block group overflow-hidden"
+              >
+                <Image
+                  src={`https://img.youtube.com/vi/${id}/hqdefault.jpg`}
+                  alt="Makabis & Benet YouTube video"
+                  fill
+                  className="object-cover transition-transform duration-300 group-hover:scale-105"
+                />
+                {/* Play icon overlay */}
+                <div className="absolute inset-0 flex items-center justify-center bg-black/30 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 68 48"
+                    width="36"
+                    height="25"
+                    fill="none"
+                  >
+                    <path
+                      d="M66.5 7.5C65.7 4.7 63.5 2.5 60.7 1.7 55.4 0 34 0 34 0S12.6 0 7.3 1.7C4.5 2.5 2.3 4.7 1.5 7.5 0 13 0 24 0 24s0 11 1.5 16.5c.8 2.8 3 5 5.8 5.8C12.6 48 34 48 34 48s21.4 0 26.7-1.7c2.8-.8 5-3 5.8-5.8C68 35 68 24 68 24s0-11-1.5-16.5z"
+                      fill="#FF0000"
+                    />
+                    <path d="M27 34l18-10-18-10v20z" fill="#fff" />
+                  </svg>
+                </div>
+              </a>
+            ))}
           </div>
         </div>
       </div>
